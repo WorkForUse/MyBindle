eab4618de59499241eedc744888ec24a6b81ea24
WorkForUse <forwork.hs2@gmail.com>
2025-06-22 19:04:17 -0700

    fix errors

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: process.env.VITE_BASE_PATH || "/MyBindle"
-// })
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/MyBindle/' : '/',
-});
+  base: "/MyBindle"
+})
